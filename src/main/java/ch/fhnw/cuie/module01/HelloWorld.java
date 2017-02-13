7aed92bde0a4e16a369ad8de2ae76a43ed7ba150
dieter <dieter.holz@fhnw.ch>
2017-02-13 18:08:00 +0100

    intitial commit

---
+++ src/main/java/ch/fhnw/cuie/module01/HelloWorld.java
@@ -0,0 +1,34 @@
+package ch.fhnw.cuie.module01;
+
+/**
+ * @author Dieter Holz
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class HelloWorld extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button button = new Button("Hello World");
+
+        StackPane rootPane = new StackPane();
+        rootPane.getChildren().add(button);
+
+        Scene myScene = new Scene(rootPane);
+
+	    primaryStage.setTitle("JavaFX App");
+        primaryStage.setScene(myScene);
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(300);
+        primaryStage.show();
+	    primaryStage.centerOnScreen();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
