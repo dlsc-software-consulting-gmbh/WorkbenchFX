77f7be595be3e1368ddbacc4cd477daf609c3919
dieter <dieter.holz@fhnw.ch>
2017-02-14 18:17:34 +0100

    setup improved

---
+++ src/test/java/ch/fhnw/cuie/ProjectSetupTest.java
@@ -0,0 +1,21 @@
+package ch.fhnw.cuie;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Dieter Holz
+ */
+public class ProjectSetupTest {
+
+    @Test
+    public void testJunitConfiguration(){
+        //given
+
+        //when
+
+        //then
+
+        Assert.assertTrue(1 == 1);
+    }
+}
