3abe87455234e8717e200399cbf1d0c1d3477e98
dieter <dieter.holz@fhnw.ch>
2017-09-19 10:33:00 +0200

    minor

--- src/main/java/ch/fhnw/oop2/module01/Person.java
+++ src/main/java/ch/fhnw/oop2/module01/Person.java
@@ -4,43 +4,43 @@ package ch.fhnw.oop2.module01;
  * @author Dieter Holz
  */
 public class Person {
-	private int    age;
-	private double weight;
-	private double height;
+    private int    age;
+    private double weight;
+    private double height;
 
-	public Person(int age, double weight, double height) {
-		this.age = age;
-		this.weight = weight;
-		this.height = height;
-	}
+    public Person(int age, double weight, double height) {
+        this.age    = age;
+        this.weight = weight;
+        this.height = height;
+    }
 
-	public double getBMI() {
-		return weight / (height * height);
-	}
+    public double getBMI() {
+        return weight / (height * height);
+    }
 
-	//alle Getter und Setter
+    //alle Getter und Setter
 
-	public int getAge() {
-		return age;
-	}
+    public int getAge() {
+        return age;
+    }
 
-	public void setAge(int age) {
-		this.age = age;
-	}
+    public void setAge(int age) {
+        this.age = age;
+    }
 
-	public double getWeight() {
-		return weight;
-	}
+    public double getWeight() {
+        return weight;
+    }
 
-	public void setWeight(double weight) {
-		this.weight = weight;
-	}
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
 
-	public double getHeight() {
-		return height;
-	}
+    public double getHeight() {
+        return height;
+    }
 
-	public void setHeight(double height) {
-		this.height = height;
-	}
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
