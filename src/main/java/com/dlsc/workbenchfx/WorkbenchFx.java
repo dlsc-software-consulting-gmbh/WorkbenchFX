0673d9bf7b7fca168b5b3f07d402914a57469b44
martinfrancois <fracoisma@hotmail.com>
2018-03-13 14:07:36 +0100

    made workbenchfx be a borderpane instead of control, since it's not being used

--- src/main/java/com/dlsc/workbenchfx/WorkbenchFx.java
+++ src/main/java/com/dlsc/workbenchfx/WorkbenchFx.java
@@ -3,7 +3,7 @@ package com.dlsc.workbenchfx;
 import com.dlsc.workbenchfx.model.WorkbenchFxModel;
 import com.dlsc.workbenchfx.view.WorkbenchFxPresenter;
 import com.dlsc.workbenchfx.view.WorkbenchFxView;
-import javafx.scene.control.Control;
+import javafx.scene.layout.BorderPane;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,7 +13,7 @@ import org.apache.logging.log4j.Logger;
  * @author François Martin
  * @author Marco Sanfratello
  */
-public class WorkbenchFx extends Control {
+public class WorkbenchFx extends BorderPane {
   private static final Logger LOGGER =
       LogManager.getLogger(WorkbenchFx.class.getName());
 
