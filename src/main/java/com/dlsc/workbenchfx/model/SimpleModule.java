b69c0259e2caea35282577887813663ea1110bef
martinfrancois <fracoisma@hotmail.com>
2018-04-04 16:41:51 +0200

    renamed ModuleBase to SimpleModule, maybe some refactoring in the future added simple implementation of a module

--- src/main/java/com/dlsc/workbenchfx/model/SimpleModule.java
+++ src/main/java/com/dlsc/workbenchfx/model/SimpleModule.java
@@ -5,24 +5,30 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * Created by François Martin on 13.03.18.
  */
-public class ModuleBase implements Modular {
+public class SimpleModule implements Modular {
 
   StringProperty name = new SimpleStringProperty();
   ObjectProperty<Image> icon = new SimpleObjectProperty<>();
 
-  public ModuleBase(String name, Image icon, Node main) {
+  public SimpleModule(String name, Image icon, Node main) {
     this.name.setValue(name);
     this.icon.setValue(icon);
     mainNode.set(main);
+    Button button = new Button(name);
+    button.setGraphic(new ImageView(this.icon.get()));
+    buttonNode.setValue(button);
+    tabNode.setValue(button);
   }
 
-  public static ModuleBase of(String name, Image icon, Node main) {
-    return new ModuleBase(name, icon, main);
+  public static SimpleModule of(String name, Image icon, Node main) {
+    return new SimpleModule(name, icon, main);
   }
 
   @Override
