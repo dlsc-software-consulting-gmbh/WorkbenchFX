bc1467ddeb05f73a50cb886a556e0ca671ad6def
martinfrancois <fracoisma@hotmail.com>
2018-04-05 12:11:42 +0200

    start to form abstractmodule by introducing methods to be overriden by Module and adding the three main nodes

--- src/main/java/com/dlsc/workbenchfx/model/AbstractModule.java
+++ src/main/java/com/dlsc/workbenchfx/model/AbstractModule.java
@@ -1,5 +1,6 @@
 package com.dlsc.workbenchfx.model;
 
+import com.dlsc.workbenchfx.WorkbenchFx;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
@@ -14,9 +15,72 @@ import javafx.scene.image.ImageView;
  */
 public abstract class AbstractModule implements Module {
 
-  StringProperty name = new SimpleStringProperty();
-  ObjectProperty<Image> icon = new SimpleObjectProperty<>();
+  protected Node tile;
+  protected Node tab;
+  protected Node view;
+/*  StringProperty name = new SimpleStringProperty();
+  ObjectProperty<Image> icon = new SimpleObjectProperty<>();*/
 
+  /**
+   * Superconstructor to be called by the implementing class.
+   * @param tile
+   * @param tab
+   * @param view
+   */
+  protected AbstractModule(Node tile, Node tab, Node view) {
+    this.tile = tile;
+    this.tab = tab;
+    this.view = view;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Node getTab() {
+    return null;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Node getTile() {
+    return null;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public Node init(WorkbenchFx workbench) {
+    return null;
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void activate() {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void deactivate() {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void destroy() {
+
+  }
+/*
   public AbstractModule(String name, Image icon, Node main) {
     this.name.setValue(name);
     this.icon.setValue(icon);
@@ -25,15 +89,6 @@ public abstract class AbstractModule implements Module {
     button.setGraphic(new ImageView(this.icon.get()));
     buttonNode.setValue(button);
     tabNode.setValue(button);
-  }
+  }*/
 
-  @Override
-  public void start() {
-
-  }
-
-  @Override
-  public void stop() {
-
-  }
 }
