3302dff204bc46db8b6454223f8d6aed6336d3ef
Genron <marco.sanfratello@students.fhnw.ch>
2018-04-10 14:46:36 +0200

    a click on the home button changes to the home screen

--- src/main/java/com/dlsc/workbenchfx/view/ToolBarPresenter.java
+++ src/main/java/com/dlsc/workbenchfx/view/ToolBarPresenter.java
@@ -2,12 +2,38 @@ package com.dlsc.workbenchfx.view;
 
 import com.dlsc.workbenchfx.model.WorkbenchFxModel;
 
-public class ToolBarPresenter implements Presenter{
+public class ToolBarPresenter implements Presenter {
   private final WorkbenchFxModel model;
   private final ToolBarView view;
 
   public ToolBarPresenter(WorkbenchFxModel model, ToolBarView view) {
     this.model = model;
     this.view = view;
+    init();
   }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupEventHandlers() {
+    view.homeBtn.setOnAction(event -> model.openModule(null));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupValueChangedListeners() {
+
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void setupBindings() {
+
+  }
+
 }
