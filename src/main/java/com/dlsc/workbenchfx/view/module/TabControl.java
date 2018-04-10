30ccbdff978a7c23ee43f20f6bcd7db186bf88a9
martinfrancois <fracoisma@hotmail.com>
2018-04-10 11:22:12 +0200

    removed debug text

--- src/main/java/com/dlsc/workbenchfx/view/module/TabControl.java
+++ src/main/java/com/dlsc/workbenchfx/view/module/TabControl.java
@@ -2,10 +2,6 @@ package com.dlsc.workbenchfx.view.module;
 
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.Control;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
 
 public class TabControl extends Button {
   private final String name;
@@ -14,7 +10,7 @@ public class TabControl extends Button {
   public TabControl(String name, Node icon) {
     this.name = name;
     this.icon = icon;
-    setText(name + " Tab");
+    setText(name);
     setGraphic(icon);
   }
 
