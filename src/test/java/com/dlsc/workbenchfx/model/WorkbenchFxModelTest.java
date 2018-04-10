35d7ed7df0fdd625a6639087eb813958fd912ddd
martinfrancois <fracoisma@hotmail.com>
2018-04-10 16:15:36 +0200

    initialize all mocks

--- src/test/java/com/dlsc/workbenchfx/model/WorkbenchFxModelTest.java
+++ src/test/java/com/dlsc/workbenchfx/model/WorkbenchFxModelTest.java
@@ -1,16 +1,14 @@
 package com.dlsc.workbenchfx.model;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import com.dlsc.workbenchfx.model.module.Module;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Node;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInfo;
 
 /**
  * Created by François Martin on 20.03.18.
@@ -19,18 +17,39 @@ import org.junit.jupiter.api.TestInfo;
 class WorkbenchFxModelTest {
 
   WorkbenchFxModel model;
-  Module mockModule1 = mock(Module.class);
+
+  Module[] mockModules = new Module[3];
+  Node[] mockNodes = new Node[3];
 
   @BeforeEach
   void setUp() {
-    model = new WorkbenchFxModel();
+    // is needed to avoid "java.lang.IllegalStateException: Toolkit not initialized"
+    JFXPanel jfxPanel = new JFXPanel();
+
+    for (int i = 0; i < mockNodes.length; i++) {
+      mockNodes[i] = mock(Node.class);
+    }
+
+    for (int i = 0; i < mockModules.length; i++) {
+      mockModules[i] = mock(Module.class);
+      when(mockModules[i].activate()).thenReturn(mockNodes[i]);
+      when(mockModules[i].destroy()).thenReturn(true);
+    }
+    model = new WorkbenchFxModel(mockModules[0], mockModules[1], mockModules[2]);
   }
 
   @Test
   void openModule() {
+
   }
 
   @Test
   void closeModule() {
+
+  }
+
+  @Test
+  void closeModuleInvalid() {
+
   }
 }
