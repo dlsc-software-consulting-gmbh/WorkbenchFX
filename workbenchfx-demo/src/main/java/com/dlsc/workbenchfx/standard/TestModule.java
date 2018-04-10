314d7f5331464625c7731a292c753bbf25d4132a
martinfrancois <fracoisma@hotmail.com>
2018-04-10 10:37:50 +0200

    changed over to passing only an Icon instead of node

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/standard/TestModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/standard/TestModule.java
@@ -3,15 +3,12 @@ package com.dlsc.workbenchfx.standard;
 import com.dlsc.workbenchfx.WorkbenchFx;
 import com.dlsc.workbenchfx.model.module.AbstractModule;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.scene.Node;
-import org.controlsfx.glyphfont.GlyphFontRegistry;
 
 public class TestModule extends AbstractModule {
 
   protected TestModule() {
-//    super("Test Module", GlyphFontRegistry.font("FontAwesome").create("\uf013"));
-    super("Test Module", new FontAwesomeIconView(FontAwesomeIcon.ANGELLIST));
+    super("Test Module", FontAwesomeIcon.ANGELLIST);
   }
 
   @Override
