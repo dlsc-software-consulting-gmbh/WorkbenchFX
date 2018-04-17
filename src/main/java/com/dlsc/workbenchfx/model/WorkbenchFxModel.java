a8bb535b20172cb8b401cdfc2760fde6b424d627
martinfrancois <fracoisma@hotmail.com>
2018-04-17 09:04:22 +0200

    fixed behavior of destroy by first deactivating the module, switching to the other module before deactivating it

--- src/main/java/com/dlsc/workbenchfx/model/WorkbenchFxModel.java
+++ src/main/java/com/dlsc/workbenchfx/model/WorkbenchFxModel.java
@@ -101,11 +101,6 @@ public class WorkbenchFxModel {
     if (i == -1) {
       throw new IllegalArgumentException("Module has not been loaded yet.");
     }
-    // attemtp to destroy module
-    if (!module.destroy()) {
-      // module should or could not be destroyed
-      return false;
-    }
     // set new active module
     Module active;
     if (openModules.size() == 1) {
@@ -118,6 +113,11 @@ public class WorkbenchFxModel {
       active = openModules.get(i - 1);
     }
     activeModule.setValue(active);
+    // attempt to destroy module
+    if (!module.destroy()) {
+      // module should or could not be destroyed
+      return false;
+    }
     return openModules.remove(module);
   }
 
