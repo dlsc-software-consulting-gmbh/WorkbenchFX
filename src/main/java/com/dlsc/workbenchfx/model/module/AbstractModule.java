75ff31475dacbd7beefd2b1fc7f3860a1c0a893d
martinfrancois <fracoisma@hotmail.com>
2018-04-22 17:11:31 +0200

    added documentation and formatted according to google checkstyle rules

--- src/main/java/com/dlsc/workbenchfx/model/module/AbstractModule.java
+++ src/main/java/com/dlsc/workbenchfx/model/module/AbstractModule.java
@@ -11,8 +11,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 /**
- * Skeletal implementation of a {@link Module}.
- * Extend this class to simply implement a new module and override methods as needed.
+ * Skeletal implementation of a {@link Module}. Extend this class to simply implement a new module
+ * and override methods as needed.
  */
 public abstract class AbstractModule implements Module {
   private final String name;
@@ -43,18 +43,16 @@ public abstract class AbstractModule implements Module {
     this.name = name;
     this.tile = new TileControl(name, new FontAwesomeIconView(icon));
     this.tab = new TabControl(name, new FontAwesomeIconView(icon));
-    tab.setOnCloseRequest(() -> workbenchModel.closeModule(this));
-    tab.setOnActiveRequest(() -> workbenchModel.openModule(this));
   }
 
   /**
    * Super constructor to be called by the implementing class.
    *
-   * @param name     of this module
+   * @param name of this module
    * @param tileIcon node to be used for the icon of the tile in the home screen
-   * @param tabIcon  node to be used for the icon of the tab
-   * @throws IllegalArgumentException if tileIcon == tabIcon, since one {@link Node} instance
-   *                                  can only be displayed once in a JavaFX scene graph.
+   * @param tabIcon node to be used for the icon of the tab
+   * @throws IllegalArgumentException if tileIcon == tabIcon, since one {@link Node} instance can
+   *     only be displayed once in a JavaFX scene graph.
    */
   protected AbstractModule(String name, Node tileIcon, Node tabIcon) {
     WorkbenchFxUtils.assertNodeNotSame(tileIcon, tabIcon);
@@ -67,9 +65,9 @@ public abstract class AbstractModule implements Module {
    * Super constructor to be called by the implementing class.
    *
    * @param tile node to be used for the tile control in the home screen
-   * @param tab  node to be used for the tab control
-   * @throws IllegalArgumentException if tile == tab, since one {@link Node} instance
-   *                                  can only be displayed once in a JavaFX scene graph.
+   * @param tab node to be used for the tab control
+   * @throws IllegalArgumentException if tile == tab, since one {@link Node} instance can only be
+   *     displayed once in a JavaFX scene graph.
    */
   protected AbstractModule(Node tile, Node tab) {
     WorkbenchFxUtils.assertNodeNotSame(tile, tab);
@@ -106,9 +104,7 @@ public abstract class AbstractModule implements Module {
    * {@inheritDoc}
    */
   @Override
-  public void deactivate() {
-
-  }
+  public void deactivate() {}
 
   /**
    * {@inheritDoc}
@@ -117,5 +113,4 @@ public abstract class AbstractModule implements Module {
   public boolean destroy() {
     return true;
   }
-
 }
