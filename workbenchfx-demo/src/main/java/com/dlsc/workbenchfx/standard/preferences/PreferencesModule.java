cad475fc9a35fc06a86b8e246f133a73f55c044b
martinfrancois <fracoisma@hotmail.com>
2018-04-23 14:05:35 +0200

    removed leftovers from refactoring (imports)

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/standard/preferences/PreferencesModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/standard/preferences/PreferencesModule.java
@@ -1,6 +1,5 @@
 package com.dlsc.workbenchfx.standard.preferences;
 
-import com.dlsc.workbenchfx.model.WorkbenchFxModel;
 import com.dlsc.workbenchfx.module.AbstractModule;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.scene.Node;
