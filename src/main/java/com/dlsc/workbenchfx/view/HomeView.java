db91a49b817d6588404a4b8aa3f4faa89f6c04f5
martinfrancois <fracoisma@hotmail.com>
2018-04-24 17:23:27 +0200

    change page factory over to the one from the workbench

--- src/main/java/com/dlsc/workbenchfx/view/HomeView.java
+++ src/main/java/com/dlsc/workbenchfx/view/HomeView.java
@@ -1,11 +1,9 @@
 package com.dlsc.workbenchfx.view;
 
 import com.dlsc.workbenchfx.WorkbenchFx;
-import com.dlsc.workbenchfx.module.Module;
 import javafx.scene.Node;
 import javafx.scene.control.Pagination;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 public class HomeView extends StackPane implements View {
@@ -35,7 +33,7 @@ public class HomeView extends StackPane implements View {
   public void initializeParts() {
     int pageCount = model.getModules().size() / model.MODULES_PER_PAGE + 1;
     Pagination pagination = new Pagination(pageCount);
-    pagination.setPageFactory(this::createPage);
+    pagination.setPageFactory(model::getPage);
     pagination.setMaxPageIndicatorCount(100);
     pagination.getStyleClass().add(Pagination.STYLE_CLASS_BULLET);
 
@@ -46,35 +44,6 @@ public class HomeView extends StackPane implements View {
     AnchorPane.setBottomAnchor(pagination, 60.0);
     AnchorPane.setLeftAnchor(pagination, 10.0);
     tilePane.getChildren().addAll(pagination);
-  }
-
-  private Node createPage(int pageIndex) {
-    int COLUMNS_PER_ROW = 3;
-
-    GridPane gridPane = new GridPane();
-    gridPane.getStyleClass().add("tilePage");
-
-    int position = pageIndex * model.MODULES_PER_PAGE;
-    int count = 0;
-    int column = 0;
-    int row = 0;
-
-    while (count < model.MODULES_PER_PAGE && position < model.getModules().size()) {
-      Module module = model.getModules().get(position);
-      Node tile = model.getTile(module);
-      gridPane.add(tile, column, row);
-
-      position++;
-      count++;
-      column++;
-
-      if (column == COLUMNS_PER_ROW) {
-        column = 0;
-        row++;
-      }
-    }
-
-    return gridPane;
   }
 
   /**
