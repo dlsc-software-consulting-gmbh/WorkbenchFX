4848885a776edb447e89a98c4b75e532542f9416
martinfrancois <fracoisma@hotmail.com>
2018-04-25 23:15:04 +0200

    added glass pane

---
+++ src/main/java/com/dlsc/workbenchfx/view/controls/GlassPane.java
@@ -0,0 +1,50 @@
+package com.dlsc.workbenchfx.view.controls;
+
+import com.dlsc.workbenchfx.WorkbenchFx;
+import javafx.animation.FadeTransition;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
+
+class GlassPane extends StackPane {
+
+  public GlassPane(WorkbenchFx workbench) {
+    getStyleClass().add("glass-pane");
+
+    setMouseTransparent(false);
+    setOnMouseClicked(evt -> workbench.removeAllOverlays());
+    setVisible(false);
+
+    hideProperty().addListener(it -> {
+
+      setVisible(true);
+
+      FadeTransition fadeTransition = new FadeTransition();
+      fadeTransition.setDuration(Duration.millis(200));
+      fadeTransition.setNode(this);
+      fadeTransition.setFromValue(this.isHide() ? .5 : 0);
+      fadeTransition.setToValue(this.isHide() ? 0 : .5);
+      fadeTransition.setOnFinished(evt -> {
+        if (isHide()) {
+          setVisible(false);
+        }
+      });
+      fadeTransition.play();
+    });
+  }
+
+  private final BooleanProperty hide = new SimpleBooleanProperty(this, "hide");
+
+  public final BooleanProperty hideProperty() {
+    return hide;
+  }
+
+  public final void setHide(boolean hide) {
+    this.hide.set(hide);
+  }
+
+  public final boolean isHide() {
+    return hide.get();
+  }
+}
