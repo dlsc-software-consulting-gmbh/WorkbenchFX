8e8b6422319c6c9c0420da458505fa002807db60
martinfrancois <fracoisma@hotmail.com>
2018-04-27 08:52:30 +0200

    refactor menuitems into constructor

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/DemoPane.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/DemoPane.java
@@ -89,41 +89,41 @@ public class DemoPane extends StackPane {
     return gridPane;
   };
 
-  // Navigation Drawer
-  Menu menu1 = new Menu("Customer");
-  Menu menu2 = new Menu("Tariff Management");
-  Menu menu3 = new Menu("Complaints");
-
-  MenuItem item11 = new MenuItem("Item 1.1");
-  MenuItem item12 = new MenuItem("Item 1.2");
-  MenuItem item13 = new MenuItem("Item 1.3");
-  MenuItem item14 = new MenuItem("Item 1.4");
-
-  Menu item21 = new Menu("Item 2.1");
-  MenuItem item22 = new MenuItem("Item 2.2");
-
-  MenuItem item211 = new MenuItem("Item 2.1.1");
-  MenuItem item212 = new MenuItem("Item 2.1.2");
-  MenuItem item213 = new MenuItem("Item 2.1.3");
-  MenuItem item214 = new MenuItem("Item 2.1.4");
-  MenuItem item215 = new MenuItem("Item 2.1.5");
-
-  MenuItem item31 = new MenuItem("Item 3.1");
-  MenuItem item32 = new MenuItem("Item 3.2");
-  MenuItem item33 = new MenuItem("Item 3.3");
-
-  MenuItem itemA = new MenuItem("Complaints");
-  MenuItem itemB = new MenuItem("Printing");
-  MenuItem itemC = new MenuItem("Settings");
-
-        item21.getItems().addAll(item211,item212, item213, item214, item215);
-
-        menu1.getItems().addAll(item11, item12, item13, item14);
-        menu2.getItems().addAll(item21, item22);
-        menu3.getItems().addAll(item31, item32, item33);
-
   public DemoPane() {
+    // Navigation Drawer
+    Menu menu1 = new Menu("Customer");
+    Menu menu2 = new Menu("Tariff Management");
+    Menu menu3 = new Menu("Complaints");
 
+    MenuItem item11 = new MenuItem("Item 1.1");
+    MenuItem item12 = new MenuItem("Item 1.2");
+    MenuItem item13 = new MenuItem("Item 1.3");
+    MenuItem item14 = new MenuItem("Item 1.4");
+
+    Menu item21 = new Menu("Item 2.1");
+    MenuItem item22 = new MenuItem("Item 2.2");
+
+    MenuItem item211 = new MenuItem("Item 2.1.1");
+    MenuItem item212 = new MenuItem("Item 2.1.2");
+    MenuItem item213 = new MenuItem("Item 2.1.3");
+    MenuItem item214 = new MenuItem("Item 2.1.4");
+    MenuItem item215 = new MenuItem("Item 2.1.5");
+
+    MenuItem item31 = new MenuItem("Item 3.1");
+    MenuItem item32 = new MenuItem("Item 3.2");
+    MenuItem item33 = new MenuItem("Item 3.3");
+
+    MenuItem itemA = new MenuItem("Complaints");
+    MenuItem itemB = new MenuItem("Printing");
+    MenuItem itemC = new MenuItem("Settings");
+
+    item21.getItems().addAll(item211,item212, item213, item214, item215);
+
+    menu1.getItems().addAll(item11, item12, item13, item14);
+    menu2.getItems().addAll(item21, item22);
+    menu3.getItems().addAll(item31, item32, item33);
+
+    // WorkbenchFX
     workbenchFx = WorkbenchFx.builder(
         new CalendarModule(),
         new NotesModule(),
@@ -134,7 +134,7 @@ public class DemoPane extends StackPane {
         .pageFactory(pageFactory)
         .globalMenuFactory(globalMenuFactory)
         .navigationDrawer(menu1, menu2, menu3, itemA, itemB, itemC)
-        .build(menu1, menu2, menu3, itemA, itemB, itemC);
+        .build();
     getChildren().add(workbenchFx);
   }
 
