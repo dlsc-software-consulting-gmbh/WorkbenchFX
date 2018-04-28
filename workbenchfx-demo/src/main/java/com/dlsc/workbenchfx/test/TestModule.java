55a79e384c4e6b411b68dd7a815b1a884e6462a6
Genron <marco.sanfratello@students.fhnw.ch>
2018-04-28 12:29:43 +0200

    support of removing button and item in the TestModule

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/test/TestModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/test/TestModule.java
@@ -9,20 +9,29 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TestModule extends AbstractModule {
     private int index = 1;
 
     private final Button addMenuBtn = new Button("Add new Dropdown");
     private final Button addItemBtn = new Button("Add MenuItem to Dropdown");
+    private final Button removeMenuBtn = new Button("Remove new Dropdown");
+    private final Button removeItemBtn = new Button("Remove MenuItem from Dropdown");
     private final Dropdown customDropdown = Dropdown.of(
             "New MenuButton",
             new FontAwesomeIconView(FontAwesomeIcon.EXCLAMATION)
     );
+    private final List<MenuItem> itemsLst = new ArrayList<>();
 
-    private final HBox customPane = new HBox();
+    private final GridPane customPane = new GridPane();
 
     public TestModule() {
         super("Test Module", FontAwesomeIcon.QUESTION);
@@ -31,18 +40,31 @@ public class TestModule extends AbstractModule {
     }
 
     private void layoutParts() {
-        customPane.getChildren().addAll(addMenuBtn, addItemBtn);
+        customPane.add(addMenuBtn, 0, 0);
+        customPane.add(addItemBtn, 0, 1);
+        customPane.add(removeItemBtn, 1, 1);
+        customPane.add(removeMenuBtn, 1, 0);
         customPane.setAlignment(Pos.CENTER);
     }
 
     private void setupEventHandlers() {
         addMenuBtn.setOnAction(event -> {
             workbench.addToolBarControl(customDropdown);
-            addMenuBtn.setDisable(true);
         });
 
         addItemBtn.setOnAction(
-                event -> customDropdown.getItems().add(new CustomMenuItem(new Label("New Item " + index++)))
+                event -> {
+                    itemsLst.add(new CustomMenuItem(new Label("New Item " + index++)));
+                    customDropdown.getItems().add(itemsLst.get(itemsLst.size() - 1));
+                }
+        );
+
+        removeMenuBtn.setOnAction(event -> {
+            workbench.removeToolBarControl(customDropdown);
+        });
+
+        removeItemBtn.setOnAction(
+                event -> customDropdown.getItems().remove(itemsLst.remove(itemsLst.size() - 1))
         );
     }
 
