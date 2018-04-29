634a32b97cb118437a143b9990675c0c051d7916
martinfrancois <fracoisma@hotmail.com>
2018-04-29 17:32:26 +0200

    added comments and refactored for better readability

--- src/main/java/com/dlsc/workbenchfx/view/controls/GlassPane.java
+++ src/main/java/com/dlsc/workbenchfx/view/controls/GlassPane.java
@@ -18,8 +18,9 @@ import javafx.util.Duration;
 public class GlassPane extends StackPane {
 
   /**
-   * TODO
-   * @param workbench
+   * Creates a {@link GlassPane} object and fully initializes it.
+   * @param workbench to be used for calling {@link WorkbenchFx#hideAllOverlays()} on, when the
+   *                  {@link GlassPane} is being clicked on by the user.
    */
   public GlassPane(WorkbenchFx workbench) {
     getStyleClass().add("glass-pane");
@@ -28,24 +29,21 @@ public class GlassPane extends StackPane {
     setOnMouseClicked(evt -> workbench.hideAllOverlays());
     setVisible(false);
 
-    hideProperty()
-        .addListener(
-            it -> {
-              setVisible(true);
+    hideProperty().addListener(it -> {
+      setVisible(true);
 
-              FadeTransition fadeTransition = new FadeTransition();
-              fadeTransition.setDuration(Duration.millis(200));
-              fadeTransition.setNode(this);
-              fadeTransition.setFromValue(this.isHide() ? .5 : 0);
-              fadeTransition.setToValue(this.isHide() ? 0 : .5);
-              fadeTransition.setOnFinished(
-                  evt -> {
-                    if (isHide()) {
-                      setVisible(false);
-                    }
-                  });
-              fadeTransition.play();
-            });
+      FadeTransition fadeTransition = new FadeTransition();
+      fadeTransition.setDuration(Duration.millis(200));
+      fadeTransition.setNode(this);
+      fadeTransition.setFromValue(this.isHide() ? .5 : 0);
+      fadeTransition.setToValue(this.isHide() ? 0 : .5);
+      fadeTransition.setOnFinished(evt -> {
+        if (isHide()) {
+          setVisible(false);
+        }
+      });
+      fadeTransition.play();
+    });
   }
 
   private final BooleanProperty hide = new SimpleBooleanProperty(this, "hide");
