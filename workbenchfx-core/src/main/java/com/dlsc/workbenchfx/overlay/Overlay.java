fa3caba367abbf9cc27624971c04fc05151ca45c
martinfrancois <fracoisma@hotmail.com>
2018-05-01 17:37:48 +0200

    added javadoc and init method to interface

--- workbenchfx-core/src/main/java/com/dlsc/workbenchfx/overlay/Overlay.java
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/overlay/Overlay.java
@@ -1,10 +1,22 @@
 package com.dlsc.workbenchfx.overlay;
 
+import com.dlsc.workbenchfx.WorkbenchFx;
+import javafx.scene.Node;
+
 /**
- * TODO
+ * Represents an overlay, which can be displayed stacked on top of the view of WorkbenchFX.
+ *
+ * @author François Martin
+ * @author Marco Sanfratello
  */
 public interface Overlay {
 
-
+  /**
+   * Gets called when the overlay is being loaded upon initialization of {@link WorkbenchFx}.
+   *
+   * @param workbench the calling workbench object
+   * @return content to be displayed in this overlay
+   */
+  Node init(WorkbenchFx workbench);
 
 }
