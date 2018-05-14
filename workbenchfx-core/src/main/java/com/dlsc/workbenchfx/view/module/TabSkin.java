0e4f94cabbaa8f9afc3271983b51410561d93593
martinfrancois <fracoisma@hotmail.com>
2018-05-14 22:28:04 +0200

    added rudimentary implementation of a skin for tab and tiles

---
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/module/TabSkin.java
@@ -0,0 +1,16 @@
+package com.dlsc.workbenchfx.view.module;
+
+import javafx.scene.control.SkinBase;
+
+public class TabSkin extends SkinBase<Tab> {
+
+  /**
+   * Creates a new {@link TabSkin} object for a corresponding {@link Tab}.
+   *
+   * @param tab the {@link Tab} for which this Skin is created
+   */
+  public TabSkin(Tab tab) {
+    super(tab);
+  }
+
+}
---
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/module/TileSkin.java
@@ -0,0 +1,16 @@
+package com.dlsc.workbenchfx.view.module;
+
+import javafx.scene.control.SkinBase;
+
+public class TileSkin extends SkinBase<Tile> {
+
+  /**
+   * Creates a new {@link TileSkin} object for a corresponding {@link Tile}.
+   *
+   * @param tile the {@link Tile} for which this Skin is created
+   */
+  public TileSkin(Tile tile) {
+    super(tile);
+  }
+
+}
