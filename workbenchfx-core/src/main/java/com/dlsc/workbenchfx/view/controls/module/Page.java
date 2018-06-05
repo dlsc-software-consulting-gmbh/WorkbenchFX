2e5246af5cb71f06531e5d8b87f17b31121c875b
martinfrancois <fracoisma@hotmail.com>
2018-06-05 09:46:44 +0200

    remove todo

--- workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/controls/module/Page.java
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/controls/module/Page.java
@@ -21,7 +21,6 @@ import org.apache.logging.log4j.Logger;
  * @author Marco Sanfratello
  */
 public class Page extends Control {
-  // TODO: change demo as well
   private static final Logger LOGGER = LogManager.getLogger(Page.class.getName());
   private final Workbench workbench;
   private final ObservableList<Module> modules;
