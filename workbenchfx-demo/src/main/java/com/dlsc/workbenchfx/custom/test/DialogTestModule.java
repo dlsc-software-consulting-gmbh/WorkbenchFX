267e6951a1f02db73a49986706f265ca067e5506
martinfrancois <fracoisma@hotmail.com>
2018-06-24 23:52:54 +0200

    reflect changes from builder in demo

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/DialogTestModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/DialogTestModule.java
@@ -19,6 +19,7 @@ import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
@@ -103,8 +104,9 @@ public class DialogTestModule extends WorkbenchModule implements MapComponentIni
     longTitleBtn.setOnAction(event -> getWorkbench().showInformationDialog("Filming started 2 December 1939. The film recorded a loss of $104,000. Ikrandraco (\"Ikran dragon\") is a genus of pteranodontoid pterosaur known from Lower Cretaceous rocks in northeastern China. It is notable for its unusual skull, which features a crest on the lower jaw. Ikrandraco is based on IVPP V18199, a partial skeleton including the skull and jaws, several neck vertebrae, a partial sternal plate, parts of both wings, and part of a foot.", "You can relax, nothing wrong here."));
     longMessageBtn.setOnAction(event -> getWorkbench().showInformationDialog("Everything is fine", "Filming started 2 December 1939. The film recorded a loss of $104,000. Ikrandraco (\"Ikran dragon\") is a genus of pteranodontoid pterosaur known from Lower Cretaceous rocks in northeastern China. It is notable for its unusual skull, which features a crest on the lower jaw. Ikrandraco is based on IVPP V18199, a partial skeleton including the skull and jaws, several neck vertebrae, a partial sternal plate, parts of both wings, and part of a foot."));
     longTitleMessageBtn.setOnAction(event -> getWorkbench().showInformationDialog("In 2004, Bennett ruled that John Graham could be extradited to the United States for trial for the 1975 murder of Anna Mae Aquash, one of the most prominent members of the American Indian Movement. In 2007, she began proceedings on the Basi-Virk Affair where the Minister of Finance's politically appointed assistant was charged with the sale of benefits related to the province's sale of BC Rail, the publicly owned railway. The scandal came to public attention when news media filmed the RCMP conducting a search warrant inside the BC Legislature building.", "Filming started 2 December 1939. The film recorded a loss of $104,000. Ikrandraco (\"Ikran dragon\") is a genus of pteranodontoid pterosaur known from Lower Cretaceous rocks in northeastern China. It is notable for its unusual skull, which features a crest on the lower jaw. Ikrandraco is based on IVPP V18199, a partial skeleton including the skull and jaws, several neck vertebrae, a partial sternal plate, parts of both wings, and part of a foot."));
-    customSmallBtn.setOnAction(event -> getWorkbench().showCustomDialog(WorkbenchDialog.Type.INPUT,"What is your favorite item?", checkListView));
-    customFullBtn.setOnAction(event -> getWorkbench().showCustomDialog(WorkbenchDialog.Type.INFORMATION, "Map Overview", mapView));
+    customSmallBtn.setOnAction(event -> getWorkbench().showDialog(WorkbenchDialog.Type.INPUT,"What is your favorite item?", checkListView));
+    customFullBtn.setOnAction(event -> getWorkbench().showDialog(WorkbenchDialog.builder("Map Overview", mapView, ButtonType.CLOSE).setBlocking(true).build()));
+    customFullMaxBtn.setOnAction(event -> getWorkbench().showDialog(WorkbenchDialog.builder("Map Overview", mapView, ButtonType.CLOSE).setMaximized(true).build()));
   }
 
   @Override
