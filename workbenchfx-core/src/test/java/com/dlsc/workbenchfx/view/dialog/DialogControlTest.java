0ced8e32d39277c8d86daa50429e9c3c3ec53394
martinfrancois <fracoisma@hotmail.com>
2018-06-25 21:23:56 +0200

    extended tests by a test which executes button click

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/DialogControlTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/DialogControlTest.java
@@ -3,6 +3,8 @@ package com.dlsc.workbenchfx.view.dialog;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -10,6 +12,8 @@ import static org.mockito.Mockito.when;
 
 import com.dlsc.workbenchfx.Workbench;
 import com.dlsc.workbenchfx.testing.MockDialogControl;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -35,6 +39,8 @@ class DialogControlTest extends ApplicationTest {
   private DialogControl dialogControl;
   private SimpleObjectProperty<WorkbenchDialog> dialogProperty;
 
+  private CompletableFuture<ButtonType> result = new CompletableFuture<>();
+
   @Override
   public void start(Stage stage) {
     robot = new FxRobot();
@@ -42,6 +48,7 @@ class DialogControlTest extends ApplicationTest {
     mockDialog = mock(WorkbenchDialog.class);
     buttonTypes = FXCollections.observableArrayList(BUTTON_TYPE_1);
     when(mockDialog.getButtonTypes()).thenReturn(buttonTypes);
+    when(mockDialog.getResult()).thenReturn(result);
 
     mockBench = mock(Workbench.class);
     dialogProperty = new SimpleObjectProperty<>(mockDialog);
@@ -113,6 +120,30 @@ class DialogControlTest extends ApplicationTest {
       assertEquals(BUTTON_TYPE_1.getText(), ((Button)buttons.get(0)).getText());
       assertEquals(BUTTON_TYPE_2.getText(), ((Button)buttons.get(1)).getText());
       verify(mockDialog, times(12)).getButtonTypes();
+    });
+  }
+
+  @Test
+  void testButtonActions() {
+    robot.interact(() -> {
+      // initially
+      ObservableList<Node> buttons = dialogControl.getButtons();
+      assertSame(1, buttons.size());
+      Button button = ((Button) buttons.get(0));
+      assertEquals(BUTTON_TYPE_1.getText(), button.getText());
+
+      // verify result before firing event
+      assertFalse(result.isDone());
+
+      // fire event (simulate click on button) causing setOnAction to get triggered
+      button.fire();
+      assertTrue(result.isDone());
+      try {
+        assertEquals(BUTTON_TYPE_1, result.get());
+      } catch (InterruptedException | ExecutionException e) {
+        fail("Could not get result of dialog!");
+      }
+      verify(mockBench).hideDialog();
     });
   }
 
