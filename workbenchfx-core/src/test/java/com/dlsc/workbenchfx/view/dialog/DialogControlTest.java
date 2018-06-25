8d5216558e13664bf6ad4a6a3ccf740dcc11d609
martinfrancois <fracoisma@hotmail.com>
2018-06-25 21:03:11 +0200

    added tests

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/DialogControlTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/DialogControlTest.java
@@ -2,7 +2,9 @@ package com.dlsc.workbenchfx.view.dialog;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -11,7 +13,9 @@ import com.dlsc.workbenchfx.testing.MockDialogControl;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
@@ -19,7 +23,8 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 
 class DialogControlTest extends ApplicationTest {
-  private static final ButtonType BUTTON_TYPE = ButtonType.YES;
+  private static final ButtonType BUTTON_TYPE_1 = ButtonType.YES;
+  private static final ButtonType BUTTON_TYPE_2 = ButtonType.APPLY;
 
   private FxRobot robot;
   private Workbench mockBench;
@@ -28,17 +33,19 @@ class DialogControlTest extends ApplicationTest {
   private ObservableList<ButtonType> buttonTypes;
 
   private DialogControl dialogControl;
+  private SimpleObjectProperty<WorkbenchDialog> dialogProperty;
 
   @Override
   public void start(Stage stage) {
     robot = new FxRobot();
 
     mockDialog = mock(WorkbenchDialog.class);
-    buttonTypes = FXCollections.observableArrayList(BUTTON_TYPE);
+    buttonTypes = FXCollections.observableArrayList(BUTTON_TYPE_1);
     when(mockDialog.getButtonTypes()).thenReturn(buttonTypes);
 
     mockBench = mock(Workbench.class);
-    when(mockBench.dialogProperty()).thenReturn(new SimpleObjectProperty<>(mockDialog));
+    dialogProperty = new SimpleObjectProperty<>(mockDialog);
+    when(mockBench.dialogProperty()).thenReturn(dialogProperty);
 
     dialogControl = new MockDialogControl();
 
@@ -52,6 +59,61 @@ class DialogControlTest extends ApplicationTest {
   @Test
   void testCtor() {
     assertFalse(dialogControl.isPickOnBounds());
+  }
+
+  @Test
+  void testListeners() {
+    robot.interact(() -> {
+      // initially
+      ObservableList<Node> buttons = dialogControl.getButtons();
+      assertSame(1, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText(), ((Button)buttons.get(0)).getText());
+      verify(mockDialog, times(2)).getButtonTypes();
+
+      // change ButtonTextUppercase to uppercase
+      dialogControl.setButtonTextUppercase(true);
+      assertSame(1, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText().toUpperCase(), ((Button)buttons.get(0)).getText());
+      verify(mockDialog, times(3)).getButtonTypes();
+
+      // add buttonType
+      buttonTypes.add(BUTTON_TYPE_2);
+      assertSame(2, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText().toUpperCase(), ((Button)buttons.get(0)).getText());
+      assertEquals(BUTTON_TYPE_2.getText().toUpperCase(), ((Button)buttons.get(1)).getText());
+      verify(mockDialog, times(4)).getButtonTypes();
+
+      // change ButtonTextUppercase back to lowercase
+      dialogControl.setButtonTextUppercase(false);
+      assertSame(2, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText(), ((Button)buttons.get(0)).getText());
+      assertEquals(BUTTON_TYPE_2.getText(), ((Button)buttons.get(1)).getText());
+      verify(mockDialog, times(5)).getButtonTypes();
+
+      // change workbench
+      dialogControl.setWorkbench(null);
+      assertSame(0, buttons.size());
+      verify(mockDialog, times(7)).getButtonTypes();
+
+      // change workbench back
+      dialogControl.setWorkbench(mockBench);
+      assertSame(2, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText(), ((Button)buttons.get(0)).getText());
+      assertEquals(BUTTON_TYPE_2.getText(), ((Button)buttons.get(1)).getText());
+      verify(mockDialog, times(9)).getButtonTypes();
+
+      // change dialog
+      dialogProperty.set(null);
+      assertSame(0, buttons.size());
+      verify(mockDialog, times(10)).getButtonTypes();
+
+      // change dialog back
+      dialogProperty.set(mockDialog);
+      assertSame(2, buttons.size());
+      assertEquals(BUTTON_TYPE_1.getText(), ((Button)buttons.get(0)).getText());
+      assertEquals(BUTTON_TYPE_2.getText(), ((Button)buttons.get(1)).getText());
+      verify(mockDialog, times(12)).getButtonTypes();
+    });
   }
 
   @Test
