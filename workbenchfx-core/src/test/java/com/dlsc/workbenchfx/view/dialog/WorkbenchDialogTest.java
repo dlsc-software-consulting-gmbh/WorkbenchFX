61d18815fab4988e76e00863bee86d4b0ed7c1ea
martinfrancois <fracoisma@hotmail.com>
2018-07-03 18:31:51 +0200

    initial working test of dialog

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/WorkbenchDialogTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/dialog/WorkbenchDialogTest.java
@@ -1,28 +1,61 @@
 package com.dlsc.workbenchfx.view.dialog;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import com.dlsc.workbenchfx.Workbench;
+import com.dlsc.workbenchfx.testing.MockDialogControl;
+import java.util.concurrent.TimeoutException;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.ApplicationTest;
 
-class WorkbenchDialogTest {
+class WorkbenchDialogTest extends ApplicationTest {
 
   private static final String TITLE = "Dialog Test Title";
   private static final String MESSAGE = "Dialog Test Message";
-  private static final Label CONTENT = new Label(MESSAGE);
+  private Label CONTENT;
   private static final WorkbenchDialog.Type TYPE = WorkbenchDialog.Type.INFORMATION;
   private WorkbenchDialog dialog;
 
-  @BeforeEach
-  void setUp() {
-    dialog = WorkbenchDialog.builder(TITLE, CONTENT, TYPE).build();
+  private FxRobot robot;
+
+  @Override
+  public void start(Stage stage) {
+    robot = new FxRobot();
+    CONTENT = new Label(MESSAGE);
+    Scene scene = new Scene(CONTENT, 100, 100);
+    stage.setScene(scene);
+    stage.show();
   }
 
   @Test
-  void testCtor() {
-    WorkbenchDialog dialogConfirmation =
-        WorkbenchDialog.builder(TITLE, CONTENT, WorkbenchDialog.Type.CONFIRMATION).build();
-    WorkbenchDialog dialogMessage = WorkbenchDialog.builder(TITLE, MESSAGE, TYPE).build();
+  void testCtorContent() {
+    robot.interact(() -> {
+      dialog = WorkbenchDialog.builder(TITLE, CONTENT, TYPE).build();
+      assertEquals(TITLE, dialog.getTitle());
+      assertEquals(CONTENT, dialog.getContent());
+      assertEquals(TYPE, dialog.getType());
+      assertEquals(TYPE.name().toLowerCase(), dialog.getStyleClass().get(0));
+    });
+  }
 
+  @Test
+  void testCtorMessage() {
+    dialog = WorkbenchDialog.builder(TITLE, MESSAGE, TYPE).build();
+    assertEquals(TITLE, dialog.getTitle());
+    assertTrue(dialog.getContent() instanceof DialogMessageContent);
+    assertEquals(TYPE, dialog.getType());
   }
 
 }
