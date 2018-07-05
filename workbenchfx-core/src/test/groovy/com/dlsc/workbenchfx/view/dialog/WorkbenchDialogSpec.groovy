a9946d14000504f618966fee0ae41b1bb3b1e1b4
martinfrancois <fracoisma@hotmail.com>
2018-07-05 15:18:06 +0200

    add groovy test sources and helloworld demo for spock

---
+++ workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/dialog/WorkbenchDialogSpec.groovy
@@ -0,0 +1,12 @@
+class WorkbenchDialogSpec extends Specification {
+    def "maximum of two numbers"(int a, int b, int c) {
+        expect:
+        Math.max(a, b) == c
+
+        where:
+        a | b | c
+        1 | 3 | 3
+        7 | 4 | 7
+        0 | 0 | 0
+    }
+}
