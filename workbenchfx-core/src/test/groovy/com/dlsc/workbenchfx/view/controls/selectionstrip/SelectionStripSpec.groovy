5a77ef354c6a6b2cbe33af14857e55504b052f03
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-13 16:12:33 +0200

    Added two dummy-tests to understand the concept of testing further

--- workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/SelectionStripSpec.groovy
+++ workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/SelectionStripSpec.groovy
@@ -54,12 +54,84 @@ class SelectionStripSpec extends ApplicationSpec {
         selectionStrip.getCellFactory() instanceof Callback<SelectionStrip, StripCell<WorkbenchModule>>
     }
 
-    /*
-    Wurde styleklasse gesetzt? korrekt?
-    prefwidth/height korrekt gesetzt?
-    wurde cellfactory aufgerufen?
-    wurde selectedItemProperty aufgerufen?
+    def "tests if setting an item sets them active the correct way"() {
+        given: "WorkbenchModule-mocks as items"
+        WorkbenchModule workbenchModule = Mock()
+        WorkbenchModule workbenchModule2 = Mock()
+        int size = selectionStrip.getItems().size()
 
+        when: "adding null as parameter"
+        robot.interact {
+            selectionStrip.getItems().add(null)
+        }
+
+        then: "the active module is null and the size of the list is 0"
+        robot.interact {
+            Objects.isNull(selectionStrip.getSelectedItem())
+            size == selectionStrip.getItems().size()
+        }
+
+        when: "adding a module"
+        robot.interact {
+            selectionStrip.getItems().add(workbenchModule)
+        }
+
+        then: "the active module is the one hand over"
+        robot.interact {
+            workbenchModule == selectionStrip.getSelectedItem()
+            size == selectionStrip.getItems().size()
+        }
+
+        when: "setting another Module"
+        robot.interact {
+            selectionStrip.getItems().add(workbenchModule2)
+        }
+
+        then: "the active module should change to the second one"
+        robot.interact {
+            workbenchModule == selectionStrip.getSelectedItem()
+            size + 1 == selectionStrip.getItems().size()
+        }
+
+        when: "setting the second module again"
+        robot.interact {
+            selectionStrip.getItems().add(workbenchModule2)
+        }
+
+        then: "it will be added and the active module should change to the second one"
+        robot.interact {
+            workbenchModule2 == selectionStrip.getSelectedItem()
+            size + 2 == selectionStrip.getItems().size()
+        }
+    }
+
+    def "bla"() {
+        given: ""
+        WorkbenchModule workbenchModule = Mock()
+        WorkbenchModule workbenchModule2 = Mock()
+
+        when: ""
+        boolean areSame = workbenchModule.equals(workbenchModule2)
+
+        then: ""
+        1 * workbenchModule.equals(_) >> true
+        areSame
+    }
+
+    def "bla2"() {
+        given: ""
+        SelectionStrip<WorkbenchModule> selectionStrip = Mock()
+        WorkbenchModule workbenchModule = Mock()
+
+        when: ""
+        WorkbenchModule module = selectionStrip.getSelectedItem()
+
+        then: ""
+        1 * selectionStrip.getSelectedItem() >> workbenchModule
+        workbenchModule == module
+    }
+
+    /*
     wurde selectedItem aufgerufen?
     wurde isAutoscrolling aufgerufen?
     wurde scrollto aufgerufen?
