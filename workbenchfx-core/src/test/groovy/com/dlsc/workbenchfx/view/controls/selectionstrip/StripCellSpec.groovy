8e4ff68aafe19006c52559fed6fb3f027aaa2dba
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-13 10:22:51 +0200

    test if styleclass was set correctly

--- workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/StripCellSpec.groovy
+++ workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/StripCellSpec.groovy
@@ -8,18 +8,31 @@ import org.testfx.framework.spock.ApplicationSpec
 
 class StripCellSpec extends ApplicationSpec {
 
-    private StripCell<WorkbenchModule> stripCell = new StripCell<>()
-    private FxRobot robot = new FxRobot()
+    private StripCell<WorkbenchModule> stripCell
+    private FxRobot robot
 
     @Override
     void start(Stage stage) throws Exception {
+        stripCell = new StripCell<>()
+        robot = new FxRobot()
+
         Scene scene = new Scene(stripCell, 100, 100)
         stage.setScene(scene)
         stage.show()
     }
 
+    def "test if styleclass was set correctly"() {
+        given: "String of styleclass which shall be set"
+        String styleClass = "strip-cell"
+
+        when: "the cell is created"
+        stripCell = new StripCell<>()
+
+        then: "styleclass must be set"
+        stripCell.getStyleClass().contains(styleClass)
+    }
+
     /*
-    Wurde styleclasse gesetzt? richtig gesetzt?
     maxwitdh? maxheight?
     listener: 4 möglichkeiten -> selectionstrip-mock -> abfragen ob gecallt worden
     sttext aufgerufen?
