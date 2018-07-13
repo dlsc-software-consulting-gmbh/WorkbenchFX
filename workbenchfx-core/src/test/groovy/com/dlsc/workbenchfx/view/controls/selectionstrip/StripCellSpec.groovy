92d7ab1ca4873e4704209c9003cc942587deea12
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-13 14:26:24 +0200

    Tried testing addListener on change of SelectionStrip in StripCell

--- workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/StripCellSpec.groovy
+++ workbenchfx-core/src/test/groovy/com/dlsc/workbenchfx/view/controls/selectionstrip/StripCellSpec.groovy
@@ -1,6 +1,8 @@
 package com.dlsc.workbenchfx.view.controls.selectionstrip
 
 import com.dlsc.workbenchfx.model.WorkbenchModule
+import javafx.beans.WeakInvalidationListener
+import javafx.beans.property.ObjectProperty
 import javafx.scene.Scene
 import javafx.stage.Stage
 import org.testfx.api.FxRobot
@@ -9,11 +11,18 @@ import org.testfx.framework.spock.ApplicationSpec
 class StripCellSpec extends ApplicationSpec {
 
     private StripCell<WorkbenchModule> stripCell
+//    ObjectProperty<WorkbenchModule> property
+//    SelectionStrip<WorkbenchModule> selectionStrip
     private FxRobot robot
 
     @Override
     void start(Stage stage) throws Exception {
         stripCell = new StripCell<>()
+//        property = Mock()
+//        property.addListener(_ as WeakInvalidationListener)
+//        selectionStrip = Mock()
+//        selectionStrip.selectedItemProperty() >> property
+
         robot = new FxRobot()
 
         Scene scene = new Scene(stripCell, 100, 100)
@@ -44,8 +53,53 @@ class StripCellSpec extends ApplicationSpec {
         val == stripCell.getMaxHeight()
     }
 
+//    def "Initialization of a Dialog with Type #type has exactly the ButtonTypes #buttonTypes"(
+//            Type type, ButtonType[] buttonTypes) {
+//        given:
+//        dialog = WorkbenchDialog.builder(TITLE, content, type).build()
+//
+//        expect:
+//        type == dialog.getType()
+//        buttonTypes.length == dialog.getButtonTypes().size()
+//        buttonTypes == dialog.getButtonTypes().toArray()
+//
+//        where:
+//        type              || buttonTypes
+//        Type.INPUT        || [ButtonType.OK, ButtonType.CANCEL] as ButtonType[]
+
+    def "test listener when setting the selectionStrip"() {
+        given: "SelectionStrip as Mock"
+        ObjectProperty<WorkbenchModule> property = Mock()
+        SelectionStrip<WorkbenchModule> selectionStrip = Mock()
+        selectionStrip.selectedItemProperty() >> { args -> property }
+
+        when: "initial setup"
+        robot.interact {
+            stripCell = new StripCell<>()
+        }
+
+        then: "selectionStripProperty is null"
+        Objects.isNull(stripCell.getSelectionStrip())
+
+        when: "old null and new null"
+        robot.interact {
+            stripCell.setSelectionStrip(null)
+        }
+
+        then: "nothing shall happen (selectionStripProperty is still null)"
+        Objects.isNull(stripCell.getSelectionStrip())
+
+        when: "old null and new !null"
+        robot.interact {
+            stripCell.setSelectionStrip(selectionStrip)
+        }
+
+        then: ""
+        1 * selectionStrip.selectedItemProperty()
+        1 * property.addListener(_ as WeakInvalidationListener)
+    }
+
     /*
-    maxwitdh? maxheight?
     listener: 4 möglichkeiten -> selectionstrip-mock -> abfragen ob gecallt worden
     sttext aufgerufen?
     getitem.toString aufgerufen?
