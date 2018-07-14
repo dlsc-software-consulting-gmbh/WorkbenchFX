45a1e293ca1b92afac8a5f73590363aec23b8e6c
martinfrancois <fracoisma@hotmail.com>
2018-07-14 17:14:09 +0200

    adjusted test module to work with new structure

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/DialogTestModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/DialogTestModule.java
@@ -109,7 +109,8 @@ public class DialogTestModule extends WorkbenchModule implements MapComponentIni
     longTitleMessageBtn.setOnAction(event -> getWorkbench().showInformationDialog("In 2004, Bennett ruled that John Graham could be extradited to the United States for trial for the 1975 murder of Anna Mae Aquash, one of the most prominent members of the American Indian Movement. In 2007, she began proceedings on the Basi-Virk Affair where the Minister of Finance's politically appointed assistant was charged with the sale of benefits related to the province's sale of BC Rail, the publicly owned railway. The scandal came to public attention when news media filmed the RCMP conducting a search warrant inside the BC Legislature building.", "Filming started 2 December 1939. The film recorded a loss of $104,000. Ikrandraco (\"Ikran dragon\") is a genus of pteranodontoid pterosaur known from Lower Cretaceous rocks in northeastern China. It is notable for its unusual skull, which features a crest on the lower jaw. Ikrandraco is based on IVPP V18199, a partial skeleton including the skull and jaws, several neck vertebrae, a partial sternal plate, parts of both wings, and part of a foot."));
     customSmallBtn.setOnAction(event -> {
       WorkbenchDialog libraryDialog = WorkbenchDialog.builder("Select your favorite libraries", checkListView, WorkbenchDialog.Type.INPUT).build();
-      CompletableFuture<ButtonType> result = getWorkbench().showDialog(libraryDialog);
+      getWorkbench().showDialog(libraryDialog);
+      CompletableFuture<ButtonType> result = libraryDialog.getResult();
       result.thenAccept(buttonType -> {
         if (ButtonType.CANCEL.equals(buttonType)) {
           System.err.println("Dialog was cancelled!");
@@ -123,14 +124,16 @@ public class DialogTestModule extends WorkbenchModule implements MapComponentIni
       WorkbenchDialog dialog = WorkbenchDialog.builder("Map Overview", mapView, ButtonType.FINISH)
               .maximized(true)
               .build();
-      CompletableFuture<ButtonType> dialogResult = getWorkbench().showDialog(dialog);
+      getWorkbench().showDialog(dialog);
+      CompletableFuture<ButtonType> dialogResult = dialog.getResult();
       dialogResult.thenAccept(buttonType -> System.err.println("Dialog result: " + buttonType));
     });
     noButtonsBtn.setOnAction(event -> {
       WorkbenchDialog dialog = WorkbenchDialog.builder("This dialog has no buttons","Click outside of the dialog to close it.", WorkbenchDialog.Type.INFORMATION)
           .showButtonsBar(false)
           .build();
-      CompletableFuture<ButtonType> dialogResult = getWorkbench().showDialog(dialog);
+      getWorkbench().showDialog(dialog);
+      CompletableFuture<ButtonType> dialogResult = dialog.getResult();
       dialogResult.thenAccept(buttonType -> System.err.println("Dialog result: " + buttonType));
     });
   }
