627b1d80ba5b0fc9475c0aa3e79fbff5b6eb852c
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-20 17:32:16 +0200

    created mock-classes

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/controls/selectionstrip/TabCellTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/controls/selectionstrip/TabCellTest.java
@@ -1,11 +1,14 @@
 package com.dlsc.workbenchfx.view.controls.selectionstrip;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.dlsc.workbenchfx.Workbench;
 import com.dlsc.workbenchfx.model.WorkbenchModule;
+import com.dlsc.workbenchfx.view.controls.module.Tab;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
 
@@ -15,6 +18,8 @@ public class TabCellTest extends ApplicationTest {
   private Workbench mockBench;
   private WorkbenchModule mockModule;
   private SelectionStrip<Workbench> mockStrip;
+  private Callback<Workbench, Tab> mockFactory;
+  private Tab mockTab;
 
   private TabCell tabCell;
 
@@ -24,12 +29,20 @@ public class TabCellTest extends ApplicationTest {
     mockBench = mock(Workbench.class);
     mockModule = mock(WorkbenchModule.class);
     mockStrip = mock(SelectionStrip.class);
+    mockFactory = mock(Callback.class);
+    mockTab = mock(Tab.class);
 
     tabCell = new TabCell();
+
+    when(mockModule.getWorkbench()).thenReturn(mockBench);
+    when(mockBench.getTabFactory()).thenReturn(mockFactory);
+    when(mockFactory.call(mockBench)).thenReturn(mockTab);
 
     Scene scene = new Scene(tabCell, 100, 100);
     stage.setScene(scene);
     stage.show();
   }
+
+
 
 }
