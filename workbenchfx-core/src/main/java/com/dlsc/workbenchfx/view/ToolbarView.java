207325ed436ea610ae475b93a42bd7c5a8445aa0
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-25 17:42:12 +0200

    replaced fa-icon from menubutton with material-icon

--- workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/ToolbarView.java
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/ToolbarView.java
@@ -4,8 +4,6 @@ import static com.dlsc.workbenchfx.Workbench.STYLE_CLASS_ACTIVE_HOME;
 
 import com.dlsc.workbenchfx.model.WorkbenchModule;
 import com.dlsc.workbenchfx.view.controls.selectionstrip.SelectionStrip;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
-import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
@@ -25,10 +23,9 @@ public class ToolbarView extends VBox implements View {
   private HBox topBox;
   private HBox bottomBox;
 
-  private FontAwesomeIconView homeIconView;
-  private FontAwesomeIconView menuIconView;
   private StackPane addIconShape;
   Button addBtn;
+  private StackPane menuIconShape;
   Button menuBtn;
   SelectionStrip<WorkbenchModule> tabBar;
   HBox toolbarControlLeftBox;
@@ -59,10 +56,6 @@ public class ToolbarView extends VBox implements View {
     bottomBox = new HBox();
     bottomBox.setId("bottom-box");
 
-    homeIconView = new FontAwesomeIconView(FontAwesomeIcon.PLUS);
-    homeIconView.setId("home-icon-view");
-    homeIconView.getStyleClass().add("icon-view");
-
     addIconShape = new StackPane();
     addIconShape.getStyleClass().add("shape");
     addBtn = new Button("", addIconShape);
@@ -70,10 +63,10 @@ public class ToolbarView extends VBox implements View {
     addBtn.setId("add-button");
     addBtn.getStyleClass().add(STYLE_CLASS_ACTIVE_HOME);
 
-    menuIconView = new FontAwesomeIconView(FontAwesomeIcon.BARS);
-    menuIconView.setId("menu-icon-view");
-    menuIconView.getStyleClass().add("icon-view");
-    menuBtn = new Button("", menuIconView);
+    menuIconShape = new StackPane();
+    menuIconShape.getStyleClass().add("shape");
+    menuBtn = new Button("", menuIconShape);
+    menuBtn.getStyleClass().add("icon");
     menuBtn.setId("menu-button");
 
     tabBar = new SelectionStrip<>();
