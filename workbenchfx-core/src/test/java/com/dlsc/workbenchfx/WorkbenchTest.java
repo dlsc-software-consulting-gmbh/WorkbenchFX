d68037c746666992f63aebb132d977dadc687c7c
martinfrancois <fracoisma@hotmail.com>
2018-07-27 16:54:46 +0200

    remove tests for the functionality that was removed

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/WorkbenchTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/WorkbenchTest.java
@@ -147,7 +147,6 @@ class WorkbenchTest extends ApplicationTest {
     // Setup WorkbenchDialog Mock
     when(mockDialog.getButtonTypes()).thenReturn(buttonTypes);
     when(mockDialog.getOnResult()).thenReturn(mockOnResult);
-    when(mockDialog.getCancelDialogButtonType()).thenReturn(ButtonType.CANCEL);
 
     navigationDrawer = new MockNavigationDrawer();
     dialogControl = new MockDialogControl();
@@ -1501,7 +1500,7 @@ class WorkbenchTest extends ApplicationTest {
   }
 
   @Test
-  @DisplayName("Show non-blocking dialog and close by clicking on the GlassPane - default")
+  @DisplayName("Show non-blocking dialog and close by clicking on the GlassPane")
   void showDialogNonBlockingCloseGlassPaneDefault() {
     robot.interact(() -> {
       assertDialogNotShown();
@@ -1516,30 +1515,6 @@ class WorkbenchTest extends ApplicationTest {
       simulateGlassPaneClick(dialogControl);
 
       verify(mockOnResult).accept(ButtonType.CANCEL);
-      assertDialogNotShown();
-    });
-  }
-
-  @Test
-  @DisplayName("Show non-blocking dialog and close by clicking on the GlassPane - "
-      + "custom cancel ButtonType was defined")
-  void showDialogNonBlockingCloseGlassPaneCustom() {
-    ButtonType cancelButtonType = ButtonType.FINISH;
-    when(mockDialog.getCancelDialogButtonType()).thenReturn(cancelButtonType);
-    robot.interact(() -> {
-      assertDialogNotShown();
-
-      WorkbenchDialog result = workbench.showDialog(mockDialog);
-
-      assertDialogShown(result, false);
-      verify(mockDialog, atLeastOnce()).getButtonTypes();
-      verify(mockOnResult, never()).accept(any()); // no result yet
-
-      // hiding by GlassPane click
-      simulateGlassPaneClick(dialogControl);
-
-      verify(mockDialog).getCancelDialogButtonType();
-      verify(mockOnResult).accept(cancelButtonType);
       assertDialogNotShown();
     });
   }
