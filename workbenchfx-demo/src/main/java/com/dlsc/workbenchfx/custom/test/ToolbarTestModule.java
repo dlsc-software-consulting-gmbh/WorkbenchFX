e62a5fb1359b5672b2ae3d34ce9728326cb651ac
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-28 14:45:14 +0200

    Adjusted Demo

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/ToolbarTestModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/test/ToolbarTestModule.java
@@ -1,11 +1,12 @@
 package com.dlsc.workbenchfx.custom.test;
 
 import com.dlsc.workbenchfx.model.WorkbenchModule;
+import com.dlsc.workbenchfx.view.controls.Dropdown;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuButton;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
 import org.apache.logging.log4j.LogManager;
@@ -20,10 +21,13 @@ public class ToolbarTestModule extends WorkbenchModule {
       new Button("", new FontAwesomeIconView(FontAwesomeIcon.MINUS));
   private final Button plusBtn =
       new Button("", new FontAwesomeIconView(FontAwesomeIcon.PLUS));
-  private final MenuButton userIconBtn =
-      new MenuButton("", new FontAwesomeIconView(FontAwesomeIcon.USER),
-          new MenuItem("Content")
-      );
+  private final MenuItem addContentItem = new MenuItem("Add Content");
+  private final MenuItem removeContentItem = new MenuItem("Remove Content");
+  private final Dropdown addContentDropdown = Dropdown.of(
+      "ADD CONTENT", new FontAwesomeIconView(FontAwesomeIcon.USER),
+      addContentItem, removeContentItem
+  );
+  private int contentIndex = 1;
 
   private final VBox contentBox = new VBox();
 
@@ -32,19 +36,28 @@ public class ToolbarTestModule extends WorkbenchModule {
 
     minusBtn.setOnAction(evt -> {
       LOGGER.trace("getToolbarControlsRight().size() = " + getToolbarControlsRight().size());
-      getToolbarControlsRight().remove(userIconBtn);
+      getToolbarControlsRight().remove(addContentDropdown);
       LOGGER.trace("getToolbarControlsRight().size() = " + getToolbarControlsRight().size());
     });
     plusBtn.setOnAction(evt -> {
       LOGGER.trace("getToolbarControlsRight().size() = " + getToolbarControlsRight().size());
-      getToolbarControlsRight().add(userIconBtn);
+      getToolbarControlsRight().add(addContentDropdown);
       LOGGER.trace("getToolbarControlsRight().size() = " + getToolbarControlsRight().size());
     });
-    userIconBtn.setOnAction(evt -> {});
+    addContentItem.setOnAction(evt ->
+        contentBox.getChildren().add(new Label("Content " + contentIndex++))
+    );
+    removeContentItem.setOnAction(evt -> {
+      if (--contentIndex < 1) {
+        contentIndex = 1;
+      } else {
+        contentBox.getChildren().remove(contentIndex-1);
+      }
+    });
 
     getToolbarControlsLeft().add(minusBtn);
     getToolbarControlsLeft().add(plusBtn);
-    getToolbarControlsRight().add(userIconBtn);
+    getToolbarControlsRight().add(addContentDropdown);
 
     contentBox.getStyleClass().add("toolbar-module");
     contentBox.getStylesheets().add(
