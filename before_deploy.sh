b422d9dfb60ab0484fd7bff982a5d66b52ab12b0
martinfrancois <fracoisma@hotmail.com>
2018-07-28 19:52:01 +0200

    fix path reference for javadoc

--- before_deploy.sh
+++ before_deploy.sh
@@ -2,7 +2,7 @@
 echo "Running mvn install"
 mvn -pl "!workbenchfx-dirk" install -DskipTests
 echo "Making zips"
-cd ${TRAVIS_BUILD_DIR}/workbenchfx-core/target/site/apidocs
+cd ${TRAVIS_BUILD_DIR}/workbenchfx-core/target/apidocs
 zip -r ${TRAVIS_BUILD_DIR}/javadoc.zip .
 cd ${TRAVIS_BUILD_DIR}/target/generated-docs
 zip -r ${TRAVIS_BUILD_DIR}/documentation.zip .
