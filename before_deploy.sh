c8c47aa354ffd799ea4b5411af62941bfef0d9b3
martinfrancois <fracoisma@hotmail.com>
2018-07-28 19:03:18 +0200

    try again with scripts

--- before_deploy.sh
+++ before_deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo "Running mvn install"
 mvn -pl "!workbenchfx-dirk" install -DskipTests
+echo "Making zips"
 zip -r ${TRAVIS_BUILD_DIR}/javadoc.zip ${TRAVIS_BUILD_DIR}/workbenchfx-core/target/site
 zip -r ${TRAVIS_BUILD_DIR}/documentation.zip ${TRAVIS_BUILD_DIR}/target/generated-docs
