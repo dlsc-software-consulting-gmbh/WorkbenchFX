550b4de8ea9a1415397467740b71ca4c30fc70f7
Genron <marco.sanfratello@students.fhnw.ch>
2018-07-30 11:14:38 +0200

    Rewrote Control, so that it is possible to set a Module and listens to its Sets

--- workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/controls/ToolbarControl.java
+++ workbenchfx-core/src/main/java/com/dlsc/workbenchfx/view/controls/ToolbarControl.java
@@ -1,6 +1,10 @@
 package com.dlsc.workbenchfx.view.controls;
 
 import com.dlsc.workbenchfx.Workbench;
+import com.dlsc.workbenchfx.model.WorkbenchModule;
+import javafx.beans.InvalidationListener;
+import javafx.beans.property.SetProperty;
+import javafx.beans.property.SimpleSetProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableSet;
 import javafx.scene.Node;
@@ -8,9 +12,9 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 
 /**
- * Represents a toolbar, which displays all toolbar items of a
- * {@link com.dlsc.workbenchfx.model.WorkbenchModule}.
- * It consists of two areas to display the items: The left and the right toolbarControlBox.
+ * Represents a toolbar, which displays all toolbar items of a {@link
+ * com.dlsc.workbenchfx.model.WorkbenchModule}. It consists of two areas to display the items: The
+ * left and the right toolbarControlBox.
  *
  * @author François Martin
  * @author Marco Sanfratello
@@ -19,6 +23,15 @@ public class ToolbarControl extends HBox {
 
   private HBox toolbarControlLeftBox;
   private HBox toolbarControlRightBox;
+  private WorkbenchModule workbenchModule;
+
+  // The content of the two HBoxes listens to the two Sets and will be set on change.
+  private final SetProperty<Node> toolbarControlsLeft = new SimpleSetProperty<>(this,
+      "toolbarControlsLeft",
+      FXCollections.observableSet());
+  private final SetProperty<Node> toolbarControlsRight = new SimpleSetProperty<>(this,
+      "toolbarControlsRight",
+      FXCollections.observableSet());
 
   /**
    * Creates an empty {@link ToolbarControl} object and fully initializes it.
@@ -26,6 +39,7 @@ public class ToolbarControl extends HBox {
   public ToolbarControl() {
     initializeParts();
     layoutParts();
+    setupListeners();
   }
 
   private void initializeParts() {
@@ -46,43 +60,40 @@ public class ToolbarControl extends HBox {
   }
 
   private void layoutParts() {
-    getChildren().addAll(
-        toolbarControlLeftBox,
-        toolbarControlRightBox
-    );
+    getChildren().addAll(toolbarControlLeftBox, toolbarControlRightBox);
     HBox.setHgrow(toolbarControlLeftBox, Priority.ALWAYS);
   }
 
-  public void addToolbarControlLeft(Node toolbarControl) {
-    toolbarControlLeftBox.getChildren().add(toolbarControl);
+  private void setupListeners() {
+    toolbarControlsLeft.addListener((InvalidationListener) c ->
+        toolbarControlLeftBox.getChildren().setAll(toolbarControlsLeft));
+    toolbarControlsRight.addListener((InvalidationListener) c ->
+        toolbarControlRightBox.getChildren().setAll(toolbarControlsRight));
   }
 
-  public void removeToolbarControlLeft(Node toolbarControl) {
-    toolbarControlLeftBox.getChildren().remove(toolbarControl);
-  }
+  /**
+   * Sets a {@link WorkbenchModule} and binds this Controls Sets to the Modules toolbar items.
+   * @param workbenchModule whose toolbar items will be displayed in the Control
+   */
+  public void setModule(WorkbenchModule workbenchModule) {
+    this.workbenchModule = workbenchModule;
 
-  public void addToolbarControlRight(Node toolbarControl) {
-    toolbarControlRightBox.getChildren().add(toolbarControl);
-  }
+    // Unbind Modules, which were set before
+    toolbarControlsLeft.unbind();
+    toolbarControlsRight.unbind();
 
-  public void removeToolbarControlRight(Node toolbarControl) {
-    toolbarControlRightBox.getChildren().remove(toolbarControl);
+    // Bind new Module
+    toolbarControlsLeft.bindContent(workbenchModule.getToolbarControlsLeft());
+    toolbarControlsRight.bindContent(workbenchModule.getToolbarControlsRight());
   }
 
   /**
    * Returns whether the {@link ToolbarControl} is empty or not.
+   *
    * @return whether the {@link ToolbarControl} is empty or not
    */
   public boolean isEmpty() {
     return toolbarControlLeftBox.getChildren().isEmpty()
         && toolbarControlRightBox.getChildren().isEmpty();
-  }
-
-  /**
-   * Clears the {@link ToolbarControl}'s items
-   */
-  public void clear() {
-    toolbarControlLeftBox.getChildren().clear();
-    toolbarControlRightBox.getChildren().clear();
   }
 }
