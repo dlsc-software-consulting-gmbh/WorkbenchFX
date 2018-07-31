242854da90878a1194ff18104b5d0624f9e97584
martinfrancois <fracoisma@hotmail.com>
2018-07-31 17:19:27 +0200

    finished dark theme

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/CustomDemo.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/custom/CustomDemo.java
@@ -153,8 +153,8 @@ public class CustomDemo extends Application {
         "This will reset your device to its default factory settings.", null));
 
     // This sets the custom style. Comment this out to have a look at the default styles.
-    workbench.getStylesheets().add(CustomDemo.class.getResource("customTheme.css").toExternalForm());
-    //workbench.getStylesheets().add(CustomDemo.class.getResource("darkTheme.css").toExternalForm());
+    //workbench.getStylesheets().add(CustomDemo.class.getResource("customTheme.css").toExternalForm());
+    workbench.getStylesheets().add(CustomDemo.class.getResource("darkTheme.css").toExternalForm());
 
     workbench
         .getStylesheets()
