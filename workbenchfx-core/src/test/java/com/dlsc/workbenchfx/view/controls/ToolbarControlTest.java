4ab4268cc1995cbe235e4f2aaa67516b4410d690
Genron <marco.sanfratello@students.fhnw.ch>
2018-08-01 11:32:12 +0200

    Added test to verify that all style classes are set

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/controls/ToolbarControlTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/view/controls/ToolbarControlTest.java
@@ -1,10 +1,14 @@
 package com.dlsc.workbenchfx.view.controls;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
+
 
 /**
  * Test for {@link NavigationDrawer}.
@@ -23,6 +27,14 @@ class ToolbarControlTest extends ApplicationTest {
     Scene scene = new Scene(toolbarControl, 100, 100);
     stage.setScene(scene);
     stage.show();
+  }
+
+  @Test
+  void testSettingStyleClasses() {
+    assertTrue(toolbarControl.getStyleClass().contains("toolbar-control"));
+    assertEquals(2, toolbarControl.getChildren().size());
+    assertTrue(toolbarControl.getChildren().get(0).getStyleClass().contains("toolbar-control-left-box"));
+    assertTrue(toolbarControl.getChildren().get(1).getStyleClass().contains("toolbar-control-right-box"));
   }
 
   @Test
