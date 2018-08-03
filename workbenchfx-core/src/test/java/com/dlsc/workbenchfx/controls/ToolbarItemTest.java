3ce0e77f712145ab899369a5d9c265aff037ea89
Genron <marco.sanfratello@students.fhnw.ch>
2018-08-03 17:44:55 +0200

    added test to test constructor

--- workbenchfx-core/src/test/java/com/dlsc/workbenchfx/controls/ToolbarItemTest.java
+++ workbenchfx-core/src/test/java/com/dlsc/workbenchfx/controls/ToolbarItemTest.java
@@ -1,17 +1,19 @@
 package com.dlsc.workbenchfx.controls;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import com.dlsc.workbenchfx.view.controls.ToolbarItem;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.event.EventHandler;
 import javafx.scene.control.CustomMenuItem;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -22,11 +24,17 @@ import org.testfx.framework.junit5.ApplicationTest;
  */
 @Tag("fast")
 class ToolbarItemTest extends ApplicationTest {
+
+  private static final String TOOLBAR_BUTTON = "toolbar-button";
+  private static final String TOOLBAR_LABEL = "toolbar-label";
+  private static final String TOOLBAR_COMBO_BOX = "toolbar-menu-button";
+
   // ToolbarItem items
   private String toolbarItemText;
   private FontAwesomeIconView toolbarItemIconView;
   private ImageView toolbarItemImageView;
   private MenuItem toolbarItemMenuItem;
+  private EventHandler<? super MouseEvent> toolbarItemOnClick;
 
   private ToolbarItem toolbarItem;
 
@@ -39,44 +47,61 @@ class ToolbarItemTest extends ApplicationTest {
         new Image(ToolbarItemTest.class.getResource("../date-picker.png").toExternalForm())
     );
     toolbarItemMenuItem = new MenuItem("Menu Item");
-
-    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemIconView, toolbarItemMenuItem);
+    toolbarItemOnClick = event -> System.out.println("Item Clicked");
   }
 
   @Test
-  void createToolbarItem() {
+  void testCtors() {
+    // Default ctor
+    toolbarItem = new ToolbarItem();
+    assertTrue(toolbarItem.getStyleClass().isEmpty());
+
+    // Label ctors
     toolbarItem = new ToolbarItem(toolbarItemText);
     assertEquals(toolbarItemText, toolbarItem.getText());
-    assertNull(toolbarItem.getGraphic());
-    assertSame(0, toolbarItem.getItems().size());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_LABEL));
 
-    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemMenuItem);
-    assertEquals(toolbarItemText, toolbarItem.getText());
-    assertNull(toolbarItem.getGraphic());
-    assertSame(1, toolbarItem.getItems().size());
-
-    toolbarItem = new ToolbarItem(toolbarItemIconView);
-    assertEquals(toolbarItemIconView, toolbarItem.getGraphic());
-    assertNull(toolbarItem.getText());
-    assertSame(0, toolbarItem.getItems().size());
-
-    toolbarItem = new ToolbarItem(toolbarItemIconView, toolbarItemMenuItem);
-    assertEquals(toolbarItemIconView, toolbarItem.getGraphic());
-    assertNull(toolbarItem.getText());
-    assertSame(1, toolbarItem.getItems().size());
+    toolbarItem = new ToolbarItem(toolbarItemImageView);
+    assertEquals(toolbarItemImageView, toolbarItem.getGraphic());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_LABEL));
 
     toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemIconView);
     assertEquals(toolbarItemText, toolbarItem.getText());
     assertEquals(toolbarItemIconView, toolbarItem.getGraphic());
-    assertSame(0, toolbarItem.getItems().size());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_LABEL));
+
+    // Button ctors
+    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemOnClick);
+    assertEquals(toolbarItemText, toolbarItem.getText());
+    assertEquals(toolbarItemOnClick, toolbarItem.getOnClick());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_BUTTON));
+
+    toolbarItem = new ToolbarItem(toolbarItemImageView, toolbarItemOnClick);
+    assertEquals(toolbarItemImageView, toolbarItem.getGraphic());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_BUTTON));
+
+    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemIconView, toolbarItemOnClick);
+    assertEquals(toolbarItemText, toolbarItem.getText());
+    assertEquals(toolbarItemIconView, toolbarItem.getGraphic());
+    assertEquals(toolbarItemOnClick, toolbarItem.getOnClick());
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_BUTTON));
+
+    // MenuButton ctors
+    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemMenuItem);
+    assertEquals(toolbarItemText, toolbarItem.getText());
+    assertTrue(toolbarItem.getItems().contains(toolbarItemMenuItem));
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_COMBO_BOX));
+
+    toolbarItem = new ToolbarItem(toolbarItemImageView, toolbarItemMenuItem);
+    assertEquals(toolbarItemImageView, toolbarItem.getGraphic());
+    assertTrue(toolbarItem.getItems().contains(toolbarItemMenuItem));
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_COMBO_BOX));
 
     toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemIconView, toolbarItemMenuItem);
     assertEquals(toolbarItemText, toolbarItem.getText());
     assertEquals(toolbarItemIconView, toolbarItem.getGraphic());
-    assertSame(1, toolbarItem.getItems().size());
-
-    toolbarItem = new ToolbarItem(toolbarItemText, toolbarItemImageView, toolbarItemMenuItem);
-    assertEquals(toolbarItemImageView, toolbarItem.getGraphic());
+    assertTrue(toolbarItem.getItems().contains(toolbarItemMenuItem));
+    assertTrue(toolbarItem.getStyleClass().contains(TOOLBAR_COMBO_BOX));
   }
 
   @Test
