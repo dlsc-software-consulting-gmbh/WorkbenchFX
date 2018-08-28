c567c2b9fe7792f5b8a647f962f7473ea71ab0a1
martinfrancois <fracoisma@hotmail.com>
2018-08-28 23:03:19 +0200

    add back, forward and reload button, replace font scaling icons with more fancy ones

--- workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/modules/webview/WebModule.java
+++ workbenchfx-demo/src/main/java/com/dlsc/workbenchfx/modules/webview/WebModule.java
@@ -7,15 +7,27 @@ import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIconView;
 import java.util.Objects;
 import javafx.application.Platform;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
+import javafx.scene.shape.SVGPath;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import org.w3c.dom.Document;
 
 public class WebModule extends WorkbenchModule {
+
+  // Custom icons as SVG
+  private final SVGPath decreaseFontIcon = new SVGPath();
+  private final SVGPath increaseFontIcon = new SVGPath();
+  {
+    // materialdesignicons.com format-font-size-increase
+    increaseFontIcon.setContent("M5.12,14L7.5,7.67L9.87,14M6.5,5L1,19H3.25L4.37,16H10.62L11.75,19H14L8.5,5H6.5M18,7L13,12.07L14.41,13.5L17,10.9V17H19V10.9L21.59,13.5L23,12.07L18,7Z");
+    // materialdesignicons.com format-font-size-decrease
+    decreaseFontIcon.setContent("M5.12,14L7.5,7.67L9.87,14M6.5,5L1,19H3.25L4.37,16H10.62L11.75,19H14L8.5,5H6.5M18,17L23,11.93L21.59,10.5L19,13.1V7H17V13.1L14.41,10.5L13,11.93L18,17Z");
+  }
 
   private static final double FONT_SCALE_INCREMENT = 0.1;
   private final String url;
@@ -38,13 +50,21 @@ public class WebModule extends WorkbenchModule {
     browserUrl.setEditable(false);
 
     // setup toolbar
+    ToolbarItem back = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.CHEVRON_LEFT),
+        event -> webEngine.executeScript("history.back()"));
+    ToolbarItem forward = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.CHEVRON_RIGHT),
+        event -> webEngine.executeScript("history.forward()"));
     ToolbarItem home = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.HOME),
         event -> webEngine.load(url));
-    ToolbarItem increaseSize = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.PLUS),
+    ToolbarItem reload = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.REFRESH),
+        event -> webEngine.reload());
+
+    ToolbarItem increaseSize = new ToolbarItem(new Group(increaseFontIcon),
         event -> browser.setFontScale(browser.getFontScale() + FONT_SCALE_INCREMENT));
-    ToolbarItem decreaseSize = new ToolbarItem(new MaterialDesignIconView(MaterialDesignIcon.MINUS),
+    ToolbarItem decreaseSize = new ToolbarItem(new Group(decreaseFontIcon),
         event -> browser.setFontScale(browser.getFontScale() - FONT_SCALE_INCREMENT));
-    getToolbarControlsLeft().addAll(home, increaseSize, decreaseSize, new ToolbarItem(browserUrl));
+    getToolbarControlsLeft().addAll(back, forward, home, reload, new ToolbarItem(browserUrl));
+    getToolbarControlsRight().addAll(increaseSize, decreaseSize);
 
     // update textfield with url every time the url of the webview changes
     webEngine.documentProperty().addListener(
